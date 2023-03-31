64a409ae59ed767bb1aa2d26a02643c71afe708a
Graciliano-Thanos <guilherme.rosa.bks@gmail.com>
2023-03-31 15:58:50 -0300

    version 1.0 completed

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ import os
 if __name__ == "__main__":
     csv_folder = "csv"
     for dbf_file in os.listdir("dbf"):
-        dbfPath = "dbf/"+dbf_file
-        dbfname = dbf_file.split(".")[0]
-        print(dbfname+" in progress...")
-        csvPath = os.getcwd()+"\csv"
-        print(csvPath)
-        #df = Dbf5(dbfPath).to_dataframe()
-        #df.to_csv(csvPath+dbfname+".csv")
+        if "gitignore" not in dbf_file:
+            dbfPath = "dbf/"+dbf_file
+            dbfname = dbf_file.split(".")[0]
+            print(dbfname+" in progress...")
+            csvPath = os.getcwd()+"\csv"
+            df = Dbf5(dbfPath).to_dataframe()
+            path = csvPath+f"\{dbfname}" +".csv"
+            df.to_csv(path)
     print("Successfully converted to .csv!")
 
