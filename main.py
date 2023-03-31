544f0ac6e702b3e50a542abe7bd6b61a3940780e
Graciliano-Thanos <guilherme.rosa.bks@gmail.com>
2023-03-31 18:52:22 -0300

    adicionado documentacao simples do codigo

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ import os
 
 if __name__ == "__main__":
     csv_folder = "csv"
+
+    #Get all .dbf files in dbf directory
     for dbf_file in os.listdir("dbf"):
         if "gitignore" not in dbf_file:
             dbfPath = "dbf/"+dbf_file
             dbfname = dbf_file.split(".")[0]
             print(dbfname+"conversion in progress...")
-            csvPath = os.getcwd()+"\csv"
+            csvPath = os.getcwd()+"\csv"                 #Define .csv files path
+
+            #Convert to Dataframe then to .csv
             df = Dbf5(dbfPath).to_dataframe()
             path = csvPath+f"\{dbfname}" +".csv"
             df.to_csv(path)
